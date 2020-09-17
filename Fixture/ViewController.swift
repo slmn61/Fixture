76f3ec5e64f13c81f42d06bae1368605567eb571
selman birinci <selmanbirinci61@gmail.com>
2020-09-16 22:29:43 -0400

    Initial Commit

---
+++ Fixture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fixture
+//
+//  Created by selman birinci on 9/16/20.
+//  Copyright © 2020 birinci. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
